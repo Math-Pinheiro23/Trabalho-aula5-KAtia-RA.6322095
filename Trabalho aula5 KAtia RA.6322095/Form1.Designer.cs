16fe1aa8a938a16c3bbcb888ada5d8c01e30c229
Math2 <Math2@DESKTOP-HD5Q5DJ>
2022-09-18 18:12:39 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho aula5 KAtia RA.6322095/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Trabalho_aula5_KAtia_RA._6322095
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbl_Camisa = new System.Windows.Forms.Label();
+            this.Txt_Camisa = new System.Windows.Forms.TextBox();
+            this.Btn_Salvar = new System.Windows.Forms.Button();
+            this.Txt_Altura = new System.Windows.Forms.TextBox();
+            this.Lbl_Altura = new System.Windows.Forms.Label();
+            this.Txt_Idade = new System.Windows.Forms.TextBox();
+            this.Lbl_Idade = new System.Windows.Forms.Label();
+            this.Txt_Nome = new System.Windows.Forms.TextBox();
+            this.Lbl_Nome = new System.Windows.Forms.Label();
+            this.Txt_Peso = new System.Windows.Forms.TextBox();
+            this.Lbl_Peso = new System.Windows.Forms.Label();
+            this.Txt_Pesquisar = new System.Windows.Forms.TextBox();
+            this.Btn_Pesquisa = new System.Windows.Forms.Button();
+            this.Txt_Time = new System.Windows.Forms.TextBox();
+            this.Lbl_Time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Lbl_Camisa
+            // 
+            this.Lbl_Camisa.AutoSize = true;
+            this.Lbl_Camisa.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Lbl_Camisa.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Camisa.Location = new System.Drawing.Point(39, 562);
+            this.Lbl_Camisa.Name = "Lbl_Camisa";
+            this.Lbl_Camisa.Size = new System.Drawing.Size(108, 14);
+            this.Lbl_Camisa.TabIndex = 4;
+            this.Lbl_Camisa.Text = "Digite a Camisa";
+            // 
+            // Txt_Camisa
+            // 
+            this.Txt_Camisa.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Camisa.Location = new System.Drawing.Point(12, 579);
+            this.Txt_Camisa.Name = "Txt_Camisa";
+            this.Txt_Camisa.Size = new System.Drawing.Size(157, 22);
+            this.Txt_Camisa.TabIndex = 10;
+            // 
+            // Btn_Salvar
+            // 
+            this.Btn_Salvar.BackColor = System.Drawing.Color.Silver;
+            this.Btn_Salvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Salvar.ForeColor = System.Drawing.Color.Purple;
+            this.Btn_Salvar.Location = new System.Drawing.Point(550, 437);
+            this.Btn_Salvar.Name = "Btn_Salvar";
+            this.Btn_Salvar.Size = new System.Drawing.Size(161, 48);
+            this.Btn_Salvar.TabIndex = 12;
+            this.Btn_Salvar.Text = "Salvar";
+            this.Btn_Salvar.UseVisualStyleBackColor = false;
+            this.Btn_Salvar.Click += new System.EventHandler(this.Btn_Salvar_Click);
+            // 
+            // Txt_Altura
+            // 
+            this.Txt_Altura.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Altura.Location = new System.Drawing.Point(22, 42);
+            this.Txt_Altura.Name = "Txt_Altura";
+            this.Txt_Altura.Size = new System.Drawing.Size(157, 22);
+            this.Txt_Altura.TabIndex = 20;
+            // 
+            // Lbl_Altura
+            // 
+            this.Lbl_Altura.AutoSize = true;
+            this.Lbl_Altura.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Lbl_Altura.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Altura.Location = new System.Drawing.Point(45, 26);
+            this.Lbl_Altura.Name = "Lbl_Altura";
+            this.Lbl_Altura.Size = new System.Drawing.Size(102, 14);
+            this.Lbl_Altura.TabIndex = 19;
+            this.Lbl_Altura.Text = "Digite a Altura";
+            // 
+            // Txt_Idade
+            // 
+            this.Txt_Idade.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Idade.Location = new System.Drawing.Point(12, 506);
+            this.Txt_Idade.Name = "Txt_Idade";
+            this.Txt_Idade.Size = new System.Drawing.Size(157, 22);
+            this.Txt_Idade.TabIndex = 22;
+            // 
+            // Lbl_Idade
+            // 
+            this.Lbl_Idade.AutoSize = true;
+            this.Lbl_Idade.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Lbl_Idade.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Idade.Location = new System.Drawing.Point(39, 490);
+            this.Lbl_Idade.Name = "Lbl_Idade";
+            this.Lbl_Idade.Size = new System.Drawing.Size(97, 14);
+            this.Lbl_Idade.TabIndex = 21;
+            this.Lbl_Idade.Text = "Digite a Idade";
+            // 
+            // Txt_Nome
+            // 
+            this.Txt_Nome.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Nome.Location = new System.Drawing.Point(1002, 506);
+            this.Txt_Nome.Name = "Txt_Nome";
+            this.Txt_Nome.Size = new System.Drawing.Size(157, 22);
+            this.Txt_Nome.TabIndex = 26;
+            // 
+            // Lbl_Nome
+            // 
+            this.Lbl_Nome.AutoSize = true;
+            this.Lbl_Nome.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Lbl_Nome.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Nome.Location = new System.Drawing.Point(1035, 490);
+            this.Lbl_Nome.Name = "Lbl_Nome";
+            this.Lbl_Nome.Size = new System.Drawing.Size(99, 14);
+            this.Lbl_Nome.TabIndex = 25;
+            this.Lbl_Nome.Text = "Digite o Nome";
+            // 
+            // Txt_Peso
+            // 
+            this.Txt_Peso.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Peso.Location = new System.Drawing.Point(1002, 579);
+            this.Txt_Peso.Name = "Txt_Peso";
+            this.Txt_Peso.Size = new System.Drawing.Size(157, 22);
+            this.Txt_Peso.TabIndex = 28;
+            // 
+            // Lbl_Peso
+            // 
+            this.Lbl_Peso.AutoSize = true;
+            this.Lbl_Peso.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Lbl_Peso.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Peso.Location = new System.Drawing.Point(1035, 562);
+            this.Lbl_Peso.Name = "Lbl_Peso";
+            this.Lbl_Peso.Size = new System.Drawing.Size(92, 14);
+            this.Lbl_Peso.TabIndex = 27;
+            this.Lbl_Peso.Text = "Digite o Peso";
+            this.Lbl_Peso.Click += new System.EventHandler(this.Lbl_Peso_Click);
+            // 
+            // Txt_Pesquisar
+            // 
+            this.Txt_Pesquisar.Location = new System.Drawing.Point(550, 90);
+            this.Txt_Pesquisar.Name = "Txt_Pesquisar";
+            this.Txt_Pesquisar.Size = new System.Drawing.Size(161, 20);
+            this.Txt_Pesquisar.TabIndex = 32;
+            // 
+            // Btn_Pesquisa
+            // 
+            this.Btn_Pesquisa.BackColor = System.Drawing.Color.DarkCyan;
+            this.Btn_Pesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Pesquisa.Location = new System.Drawing.Point(550, 42);
+            this.Btn_Pesquisa.Name = "Btn_Pesquisa";
+            this.Btn_Pesquisa.Size = new System.Drawing.Size(161, 42);
+            this.Btn_Pesquisa.TabIndex = 31;
+            this.Btn_Pesquisa.Text = "Pesquisar Jogador";
+            this.Btn_Pesquisa.UseVisualStyleBackColor = false;
+            this.Btn_Pesquisa.Click += new System.EventHandler(this.Btn_Pesquisa_Click_1);
+            // 
+            // Txt_Time
+            // 
+            this.Txt_Time.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Time.Location = new System.Drawing.Point(1002, 42);
+            this.Txt_Time.Name = "Txt_Time";
+            this.Txt_Time.Size = new System.Drawing.Size(157, 22);
+            this.Txt_Time.TabIndex = 36;
+            // 
+            // Lbl_Time
+            // 
+            this.Lbl_Time.AutoSize = true;
+            this.Lbl_Time.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Lbl_Time.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Time.Location = new System.Drawing.Point(1041, 26);
+            this.Lbl_Time.Name = "Lbl_Time";
+            this.Lbl_Time.Size = new System.Drawing.Size(95, 14);
+            this.Lbl_Time.TabIndex = 35;
+            this.Lbl_Time.Text = "Digite o Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1263, 611);
+            this.Controls.Add(this.Txt_Time);
+            this.Controls.Add(this.Lbl_Time);
+            this.Controls.Add(this.Txt_Pesquisar);
+            this.Controls.Add(this.Btn_Pesquisa);
+            this.Controls.Add(this.Txt_Peso);
+            this.Controls.Add(this.Lbl_Peso);
+            this.Controls.Add(this.Txt_Nome);
+            this.Controls.Add(this.Lbl_Nome);
+            this.Controls.Add(this.Txt_Idade);
+            this.Controls.Add(this.Lbl_Idade);
+            this.Controls.Add(this.Txt_Altura);
+            this.Controls.Add(this.Lbl_Altura);
+            this.Controls.Add(this.Btn_Salvar);
+            this.Controls.Add(this.Txt_Camisa);
+            this.Controls.Add(this.Lbl_Camisa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Lbl_Camisa;
+        private System.Windows.Forms.TextBox Txt_Camisa;
+        private System.Windows.Forms.Button Btn_Salvar;
+        private System.Windows.Forms.TextBox Txt_Altura;
+        private System.Windows.Forms.Label Lbl_Altura;
+        private System.Windows.Forms.TextBox Txt_Idade;
+        private System.Windows.Forms.Label Lbl_Idade;
+        private System.Windows.Forms.TextBox Txt_Nome;
+        private System.Windows.Forms.Label Lbl_Nome;
+        private System.Windows.Forms.TextBox Txt_Peso;
+        private System.Windows.Forms.Label Lbl_Peso;
+        private System.Windows.Forms.TextBox Txt_Pesquisar;
+        private System.Windows.Forms.Button Btn_Pesquisa;
+        private System.Windows.Forms.TextBox Txt_Time;
+        private System.Windows.Forms.Label Lbl_Time;
+    }
+}
+
+
